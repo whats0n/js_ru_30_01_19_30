0997a6c776d3efe1fab324f6b073fc38fccfa68e
whats0n <mrdvv@mail.ru>
2017-02-02 19:22:59 +0200

    upd

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+import React from 'react';
+import {render} from 'react-dom';
+
+render(
+	<div>
+		<h1>Hello world</h1>
+	</div>,
+	document.getElementById('container')
+);
