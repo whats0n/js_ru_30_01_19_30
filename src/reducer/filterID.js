2f2a2d0ffe17e1afd1b01ac49defcdc605ed9bd6
Roman <kaboom.ua@gmail.com>
2017-02-15 22:51:54 +0200

    Update filterID.js

--- src/reducer/filterID.js
+++ src/reducer/filterID.js
@@ -1,5 +1,5 @@
 import {FILTER_ARTICLES_ID} from '../constants'
-
+//не дроби редюсеры слишком сильно: объедини селект и календарь в один
 export default (selection = null, action) => {
 	const {type, payload} = action;
 
