cb82e6d9c24c72935fddd33ff895e1f944820235
whats0n <mrdvv@mail.ru>
2017-02-26 19:40:37 +0200

    update

--- src/reducer/helpers.js
+++ src/reducer/helpers.js
@@ -2,6 +2,5 @@ import {Record, Map} from 'immutable'
 
 export const DefaultReducerState = Record({
     isLoading: false,
-    entities: new Map({}),
-    comments: new Map({})
+    entities: new Map({})
 })
